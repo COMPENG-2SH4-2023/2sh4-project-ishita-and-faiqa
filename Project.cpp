e4269b4ae66773cc44c94759d0bef85c94fcf697
Faiqa Anjum <faiqaanjum@Faiqas-MacBook-Air.local>
2023-11-27 16:07:07 -0500

    itr 2, still working on it

--- Project.cpp
+++ Project.cpp
@@ -55,10 +55,10 @@ void Initialize(void)
     genFood = new Food(); 
         
 
-    objPos returnPlayerPos;
-    myPlayer->getPlayerPos(returnPlayerPos);
+   // objPos returnPlayerPos;
+   // myPlayer->getPlayerPos(returnPlayerPos);
 
-    genFood -> generateFood(returnPlayerPos); // good for now, i have to wait on ishita
+   // genFood -> generateFood(returnPlayerPos); // good for now, i have to wait on ishita
 
 }
 
@@ -71,8 +71,27 @@ void RunLogic(void)
 {
     myPlayer -> updatePlayerDir(); 
     myPlayer -> movePlayer();
+    
+
+// THIS IS FOR DEBUGGING PURPOSES KEEP HERE! 
+
+/*     switch (myGM -> getInput())
+    {
+    case ' ':
+        myGM->incrementScore();
+        break;
+    case 'l':
+        myGM->setLoseFlag();
+        break;
+    default:
+        break;
+    } */  
+
+
     myGM -> clearInput(); 
+
 }
+
 
 void DrawScreen(void)
 {
@@ -110,7 +129,11 @@ void DrawScreen(void)
 
 
     MacUILib_printf("Welcome to Moving Player: Using 'W' to move UP, 'A' to move LEFT, 'S' to move DOWN, and 'D' to move RIGHT!\n"); 
-    MacUILib_printf("To exit the game, press ESC. "); 
+    MacUILib_printf("To exit the game, press ESC. \n"); 
+    MacUILib_printf("Score: %d\n\n",myGM -> getScore());
+    MacUILib_printf("Lose Flag is: %d\n\n",myGM -> getLoseFlagStatus());
+
+
 
 }
 
